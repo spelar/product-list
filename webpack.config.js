d673e3285fafccbc793e94452e26ef9bfc6bb195
Sungsu Kim <spelar7@gmail.com>
2021-01-26 23:27:29 +0900

    웹팩 설정 파일 추가, scripts 명령어 추가

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+	//웹팩이 빌드하게 될 파일
+  entry: "./src/productList.js",
+	// 빌드된 파일 위치와 네임 설정
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/build")
+  },
+	// 웹팩 개발 서버
+	devServer: {
+    contentBase: path.resolve("./build"),
+    index: "index.html",
+    port: 9000
+  },
+  mode: "production",
+	// html, css 빌드
+	module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+			{
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+			{
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+	// html, css output 설정
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html', 
+      filename: 'index.html' 
+    }),
+		new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+	// js, css 파일 압축
+	optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
